c595e2044ddf040677db3225211aa2d3c5444763
Skanlog-Ojt <Skanlog-Ojt@SKANLOG>
2023-02-16 14:52:06 +0800

    Add project files.

---
+++ CrudAPI/CommunicationModuleDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CrudAPI;
+
+namespace CommunicationModule
+{
+    public class CommunicationModuleDBContext : DbContext
+    {
+        public CommunicationModuleDBContext(DbContextOptions<CommunicationModuleDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<> People { get; set; }
+    }
+}
---
+++ CrudAPI/Startup.cs
@@ -0,0 +1,96 @@
+﻿//using CommunicationModule.DataAccess;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace CommunicationModule
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                     .RequireAuthenticatedUser()
+                     .Build();
+                //config.Filters.Add(new AuthorizeFilter(policy)); //global authorize filter
+            })
+              .AddAuthorization();
+
+
+            //services.AddAuthentication("bearer").AddJwtBearer("bearer", options =>
+            //{
+            //    options.Audience = "Skanlog.CommunicationModule.Api";
+            //    //options.Authority = "http://localhost:8080/";
+            //    options.Authority = "https://id.test.skanlog.io/";
+            //    options.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateAudience = false
+            //    };
+            //    // ignore self-signed ssl
+            //    //options.BackchannelHttpHandler = new HttpClientHandler { ServerCertificateCustomValidationCallback = delegate { return true; } };
+            //});
+            services.AddCors();
+
+            services.AddDbContextPool<CommunicationModuleDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CommunicationModuleConn")));
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommunicationModule", Version = "v1" });
+            });
+            //services.AddControllers().AddNewtonsoftJson();
+
+            //string rabbitmqConnectionString = "host=localhost;username=guest;password=guest;timeout=60";
+
+            //var bus = RabbitHutch.CreateBus(rabbitmqConnectionString);
+
+            //services.AddSingleton(bus);
+
+            //services.AddHostedService<BackgroundServices.UserEventHandler>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CommunicationModel v1"));
+            }
+
+            app.UseCors(builder => builder
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .SetIsOriginAllowed(_ => true)
+                .AllowCredentials());
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
